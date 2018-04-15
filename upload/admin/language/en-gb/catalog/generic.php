4099954a6eb6e526ab409972f8865b0cac4caa38
himanshu7891 <himanshu.er.verma@gmail.com>
2018-04-15 14:33:05 +0530

    generic

---
+++ upload/admin/language/en-gb/catalog/generic.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']     		= 'Generic Groups';
+
+// Text
+$_['text_success']      		= 'Success: You have modified generic groups!';
+$_['text_list']         		= 'Generic Group List';
+$_['text_add']          		= 'Add Generic Group';
+$_['text_edit']         		= 'Edit Generic Group';
+
+// Column
+$_['column_generic_name']       = 'Generic Name';
+$_['column_disease']       		= 'Disease';
+$_['column_how_to_use']         = 'How to use';
+$_['column_dose']       		= 'Dose';
+$_['column_warning']       		= 'Warning';
+$_['column_indication']         = 'Indication';
+$_['column_side_effect']        = 'Side Effect';
+$_['column_about_medicine']     = 'About Medicine';
+$_['column_description']        = 'Description';
+$_['column_disclaimer']         = 'Disclaimer';	
+$_['column_sort_order'] 		= 'Sort Order';
+$_['column_action']     		= 'Action';
+
+// Entry
+$_['entry_generic_name']        = 'Generic Name';
+$_['entry_disease']       		= 'Disease';
+$_['entry_how_to_use']         	= 'How to use';
+$_['entry_dose']       			= 'Dose';
+$_['entry_warning']       		= 'Warning';
+$_['entry_indication']         	= 'Indication';
+$_['entry_side_effect']        	= 'Side Effect';
+$_['entry_about_medicine']     	= 'About Medicine';
+$_['entry_description']        	= 'Description';
+$_['entry_disclaimer']         	= 'Disclaimer';
+$_['entry_sort_order']  		= 'Sort Order';
+
+// Error
+$_['error_permission']  	= 'Warning: You do not have permission to modify generic groups!';
+$_['error_generic_name']    = 'Generic Name must be between 1 and 64 characters!';
+$_['error_attribute']   	= 'Warning: This Generic Group cannot be deleted as it is currently assigned to %s attributes!';
+$_['error_product']     	= 'Warning: This Generic Group cannot be deleted as it is currently assigned to %s products!';
