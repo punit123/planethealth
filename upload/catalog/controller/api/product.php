ecba5538ca9b55f8262f4ff74a1de1ac8e6670f5
jignesh3094 <jignesh3094@gmail.com>
2018-04-18 22:19:10 +0530

    product api product fetch from category

--- upload/catalog/controller/api/product.php
+++ upload/catalog/controller/api/product.php
@@ -1,14 +1,33 @@
 <?php
 class ControllerApiProduct extends Controller {
 	public function getProductByCategories(){
+		$this->load->model('catalog/product');
 		$category_id = $this->request->post['category_id'];
-		if(isset($category_id) && $category_id != ''){
-			echo "if";	
+		$json = array();
+		if(($this->request->server['REQUEST_METHOD'] == 'POST') && $category_id != ''){
+			$fetchAllProducts = $this->model_catalog_product->getProductbyCatid($category_id);
+			if(!empty($fetchAllProducts) && count($fetchAllProducts)>0){
+				$json['status'] = 'success';
+				$json['message'] = $this->language->get('Success');
+				$json['data'] = $fetchAllProducts;
+			}
+			else{
+				$json['status'] = 'error';
+				$json['message'] = $this->language->get('No record found!');
+			}
+			
 		}
 		else{
-			echo "else";
-		}
-		echo '<pre>';print_r($category_id);die;
+				$json['status'] = 'error';
+				$json['message'] = $this->language->get('please select category id!');
+			}
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+	
+	public function getProductSearch()
+	{
+		echo "hello";
 	}
 }
 ?>
