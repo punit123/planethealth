dff753b2a16ed8cfacda3ccf4f52a3a21ed2babc
harshitdave701 <daveharshit97@gmail.com>
2018-04-18 23:57:46 +0530

    search functionality in APIs for product base search and category wise search and fetch product data

--- upload/catalog/controller/api/product.php
+++ upload/catalog/controller/api/product.php
@@ -15,14 +15,37 @@ class ControllerApiProduct extends Controller {
 				$json['status'] = 'error';
 				$json['message'] = $this->language->get('No record found!');
 			}
-			
 		}
 		else{
-				$json['status'] = 'error';
-				$json['message'] = $this->language->get('please select category id!');
-			}
+			$json['status'] = 'error';
+			$json['message'] = $this->language->get('please select category id!');
+		}
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
 	}
-}	
+	
+	public function getProductsBySearch(){
+		$this->load->model('catalog/product');
+		$search = $this->request->post['search'];
+		$json = array();
+		if(($this->request->server['REQUEST_METHOD'] == 'POST') && $search != ''){
+			$fetchAllProductsBySearch = $this->model_catalog_product->getProductsBySearch($search);
+			if(count($fetchAllProductsBySearch)>0 && !empty($fetchAllProductsBySearch)){
+				$json['status'] = 'success';
+				$json['message'] = $this->language->get('Success');
+				$json['data'] = $fetchAllProductsBySearch;
+			}
+			else{
+				$json['status'] = 'error';
+				$json['message'] = $this->language->get('no record found!');
+			}
+		}
+		else{
+			$json['status'] = 'error';
+			$json['message'] = $this->language->get('no record found!');
+		}
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+}
 ?>
