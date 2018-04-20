0de55f1331099c6902f9a3f997389fb4b494e88a
himanshu7891 <himanshu.er.verma@gmail.com>
2018-04-20 09:22:10 +0530

    custom_notification master

---
+++ upload/admin/language/en-gb/localisation/custom_notification.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']           = 'Custom Notification';
+
+// Text
+$_['text_success']            = 'Success: You have modified countries!';
+$_['text_list']               = 'Custom Notification List';
+$_['text_add']                = 'Add Custom Notification';
+$_['text_edit']               = 'Edit Custom Notification';
+
+// Column
+//$_['column_name']             = 'Custom Notification Name';
+$_['column_title']         	  = 'Title';
+$_['column_message']          = 'Message';
+$_['column_push_notification']= 'Push Notification';
+$_['column_message']          = 'Message';
+$_['column_action']           = 'Action';
+
+// Entry
+$_['entry_title']              = 'Title';
+$_['entry_message']            = 'Message';
+$_['entry_push_notification']  = 'Push Notification';
+$_['entry_email']              = 'Email';
+$_['entry_sms']                = 'SMS';
+$_['entry_status']             = 'Status';
+
+
+// Error
+$_['error_permission']        = 'Warning: You do not have permission to modify countries!';
+$_['error_title']             = 'title must be between 1 and 128 characters!';
+$_['error_message']           = 'Message must be between 1 and 128 characters!';
+$_['error_default']           = 'Warning: This faq cannot be deleted as it is currently assigned as the default store faq!';
